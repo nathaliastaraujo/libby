5d472034253fff8cae32334ab18279402b8a5438
rodriguesfelipe <feliperodriguesxz@gmail.com>
2018-06-25 22:16:46 -0300

    changes in adm/gerenciador_user.php

--- adm/gerenciador_user.php
+++ adm/gerenciador_user.php
@@ -1,6 +1,8 @@
 <?php
-	include "conexao.php"; //mudar o nome das conexões, não sei qual é
-	include "executaSQL.php"; //mudar aqui também
+
+include('../login/config.php');
+	//include "conexao.php"; //mudar o nome das conexões, não sei qual é
+	//include "executaSQL.php"; //mudar aqui também
 
 	$link=conectar(); 
 	$busca_query = mysql_query("SELECT * FROM usuario WHERE contaCod == $consultUsuario || contaEmail == $consultUsuario")or die(mysql_error());
